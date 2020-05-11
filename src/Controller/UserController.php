c4a37c0dc3e095aea2dff3077af0f1b89b6ec64f
mathieuLU <you@example.com>
2020-05-11 15:38:22 +0200

    change password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -40,6 +40,51 @@ class UserController extends Controller
     
   }
 
+
+  function getFormUser($user, $mode) {
+
+    // $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $user);
+    $formBuilder = $this->createFormBuilder($user);
+
+    $formBuilder
+      ->add('username', TextType::class, array(
+        'required'   => true,
+      ))
+      ->add('password', TextType::class, array(
+        'required'   => true,
+      ))
+      ->add('roles', ChoiceType::class, array(
+        'multiple' => true,
+        'choices' => array(
+          'ROLE_USER' => 'ROLE_USER',
+          'ROLE_ADMIN' => 'ROLE_ADMIN',
+        ),
+        'required'   => true,
+      ))
+    ;
+
+    // $form = $formBuilder->getForm();
+
+    if ($mode=="edit") {
+      $formBuilder
+      ->add('update', SubmitType::class, array('label' => 'Update'))
+      ;
+    }
+    else {
+      $formBuilder
+      // ->add('password', TextType::class, array(
+      //   'required'   => true,
+      // ))
+      ->add("Create", SubmitType::class);
+    }
+
+    $form = $formBuilder->getForm();
+
+    return $form;
+
+  }
+
+
   /**
    * @Route(
    * "/users/new", 
@@ -51,7 +96,7 @@ class UserController extends Controller
     $em = $this->getDoctrine()->getManager();
     $user = new User();
 
-    $formBuilder = $this->createFormBuilder($user);
+    /*$formBuilder = $this->createFormBuilder($user);
 
     $formBuilder
       ->add('username', TextType::class, array(
@@ -72,6 +117,9 @@ class UserController extends Controller
     ;
 
     $form = $formBuilder->getForm();
+    */
+
+    $form=$this->getFormUser($user, "add");
 
     if ($request->isMethod('POST')) {
 
@@ -103,10 +151,113 @@ class UserController extends Controller
       }
     }
 
-    return $this->render('site/user_new.html.twig', array(
+    return $this->render('site/user_form.html.twig', array(
       'form' => $form->createView(),
+      'form_title' => 'Form new user'
     ));
 
+  }
+
+  /**
+   * @Route(
+   * "/users/update/{id}", 
+   * name="user_update", 
+   * requirements={
+   *   "id"="\d+" 
+   * })
+   */
+  public function updateUserAction($id, Request $request) {
+
+    $em = $this->getDoctrine()->getManager();
+    $user = $em->getRepository('App:User')->findOneBy(['id' => $id]);
+
+    $form=$this->getFormUser($user, "edit");
+
+    if ($request->isMethod('POST')) {
+
+      $form->handleRequest($request);
+
+      if ($form->isValid()) {
+
+        $user->setPassword(password_hash($form->get('password')->getData(), PASSWORD_DEFAULT));
+
+        try {
+          // $rolesString=implode(",", $form->get('roles')->getData());
+          // $user->setRoles($rolesString);
+
+          $em->persist($user);
+          $em->flush();
+          $request->getSession()->getFlashBag()->add('success', 'User updated.');
+
+        } 
+        catch(UniqueConstraintViolationException $e) {
+          $form->get('username')->addError(new  \Symfony\Component\Form\FormError(
+            "This username already exists in the database"
+          ));
+
+        }
+      }
+    }
+
+    return $this->render('site/user_form.html.twig', array(
+      'form' => $form->createView(),
+      'form_title' => 'Update user'
+    ));
+  }
+
+  /**
+   * @Route(
+   * "/users/changepassword", 
+   * name="change_password")
+   */
+
+  public function changePasswordAction(Request $request) {
+
+    $em = $this->getDoctrine()->getManager();
+    //$user = $em->getRepository('App:User')->findOneBy(['id' => $id]);
+    $user = $this->get('security.token_storage')->getToken()->getUser();
+
+    $formBuilder = $this->createFormBuilder($user);
+
+    $formBuilder
+      ->add('password', PasswordType::class, array(
+        'required'   => true,
+      ))
+      ->add('update', SubmitType::class, array('label' => 'Change'))
+    ;
+
+    $form = $formBuilder->getForm();
+
+    if ($request->isMethod('POST')) {
+
+      $form->handleRequest($request);
+
+      if ($form->isValid()) {
+
+        $user->setPassword(password_hash($form->get('password')->getData(), PASSWORD_DEFAULT));
+
+        try {
+          // $rolesString=implode(",", $form->get('roles')->getData());
+          // $user->setRoles($rolesString);
+
+          $em->persist($user);
+          $em->flush();
+          $request->getSession()->getFlashBag()->add('success', 'Password updated.');
+
+        } 
+        catch(UniqueConstraintViolationException $e) {
+          $form->get('username')->addError(new  \Symfony\Component\Form\FormError(
+            "This username already exists in the database"
+          ));
+
+        }
+      }
+    }
+
+    return $this->render('site/user_form.html.twig', array(
+      'form' => $form->createView(),
+      'form_title' => 'Change my password'
+    ));
   }
 
 }
