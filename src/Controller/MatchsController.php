8334aea0809daacd1d78130d5807af7cd8ba1f2d
mathieuLU <you@example.com>
2020-12-02 12:37:36 +0100

    send email when tennis match added

--- src/Controller/MatchsController.php
+++ src/Controller/MatchsController.php
@@ -282,6 +282,19 @@ class MatchsController extends Controller
         $em->flush();
         $request->getSession()->getFlashBag()->add('success', 'Match created.');
 
+        $headers ='From: contact@luckylosertennis.com'."\r\n";
+        $headers .= 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+        // $headers .='Content-Type: text/html; charset="iso-8859-1"'."\r\n";
+        $headers .='Content-Transfer-Encoding: 8bit'."\r\n";
+
+        // $email_contenu = utf8_decode("et là tu les vois les accéééééénts ?");
+
+        if (mail($_SERVER['EMAIL_ADMIN'], "ATL-Stegen => tennis match created", "Match ajouté", $headers)) {}
+        else {
+          $request->getSession()->getFlashBag()->add('error', 'Error sending email to '.$_SERVER['EMAIL_ADMIN']);
+
+        }
       }
     }
 
