53407c206ce69851f832a6b779fa78fb08e3863f
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2024-10-15 00:03:58 +0200

    14th round league

--- src/Controller/MatchsController.php
+++ src/Controller/MatchsController.php
@@ -406,7 +406,7 @@ class MatchsController extends Controller
       'choices' => array(
         "Stege" => "Stege", 
         "Stege (söndag 21-22)" => "Stege (söndag 21-22)", 
-        "Division League - Round#13" => "Division League - Round#13", 
+        "Division League - Round#14" => "Division League - Round#14", 
         "A-serien (ATL)" => "A-serien", 
         "Måndagstennis (LBTK)" => "LBTK-Måndagstennis", 
         /*"Summer tournament 2024 (august)" => "Summer tournament 2024 (august)", */
