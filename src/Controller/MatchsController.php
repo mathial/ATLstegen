b8617ca70f98e329fda6eb2cfca528f2e167aa97
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2026-01-13 09:16:44 +0100

    league round 19

--- src/Controller/MatchsController.php
+++ src/Controller/MatchsController.php
@@ -408,7 +408,7 @@ class MatchsController extends Controller
       'choices' => array(
         "Stege" => "Stege", 
         "Stege (söndag 21-22)" => "Stege (söndag 21-22)", 
-        "Division League - Round#18" => "Division League - Round#18", 
+        "Division League - Round#19" => "Division League - Round#19", 
         "A-serien (ATL)" => "A-serien", 
         "Generation Matchup" => "Generation Matchup",
         "Christmas Tournament" => "Christmas Tournament", 
